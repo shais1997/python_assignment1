9d1b463ee291bb2ba290c58827748b90b22d64ec
shais1997 <57403746+shais1997@users.noreply.github.com>
2019-11-27 10:24:43 -0800

    Add files via upload

---
+++ Assignment#01.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print('Twinkle,  twinkle,  little star,')
+print('      How I wonder what you are!')
+print('            up above the world so high,')
+print('            like a diamond in the sky.')
+print('Twinkle,  twinkle,  little star,')
+print('      How I wonder what you are!')
+
+
+# In[9]:
+
+
+import sys
+print (sys.version)
+print (sys.version_info)
+
+
+# In[12]:
+
+
+import datetime
+now = datetime.datetime.now()
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+# In[19]:
+
+
+from math import pi
+r=float(input('enter the radius'))
+a=pi*r**2
+print('area of circle is' + str(a))
+
+
+# In[21]:
+
+
+fname = input("First Name : ")
+lname = input("Last Name : ")
+print ( lname + " " + fname)
+
+
+# In[ ]:
+
+
+a=int(input('enter first number'))
+b=int(input('enter sec number'))
+c=a+b
+print('sum' +str(c))
+
+
+# In[ ]:
+
+
+
+
